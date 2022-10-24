927751656aaf91b8bc58cabc9ed691142271e885
bu1avkin <alekseybulatov.study@gmail.com>
2022-10-24 20:53:00 +0300

    Задача 3 База

--- javaLessons/src/com/datatypes/idea/less3/baselvl/intType.java
+++ javaLessons/src/com/datatypes/idea/less3/baselvl/intType.java
@@ -1,7 +1,25 @@
 package com.datatypes.idea.less3.baselvl;
 
-public class intType {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class intType {
+    public static void main(String[] args) throws NumberFormatException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите кол-во чисел:");
+        int skolko = scan.nextInt();
+        for (int i = 0; i < skolko; i++) {
+            System.out.println("Введите число:");
+            float ch = scan.nextFloat();
+            if (ch % 2 == 0) {
+                System.out.println("оно чётное");
+            } else {
+                System.out.println("оно нечётное");
+            }
+            if (ch % 1 == 0) {
+                System.out.println("оно целое");
+            } else {
+                System.out.println("оно нецелое");
+            }
+        }
     }
 }
